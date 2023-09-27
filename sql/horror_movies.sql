84507d5c1cd104f26d81bfede25b580be0b70265
Chaleamchai Sathanurukwong <c.sathanurukwong@gmail.com>
2023-09-27 19:47:54 -0400

    add first code SQL

--- sql/horror_movies.sql
+++ sql/horror_movies.sql
@@ -1,1 +1,2 @@
 -- Add your SQL here
+SELECT movies.id as Movie_ID, movies.name as Movie_Title, movies.imdb_rating as Rating FROM movies WHERE genre='horror' ORDER BY movies.imdb_rating DESC LIMIT 3;
