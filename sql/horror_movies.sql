5aa2a92fffde5f24bf3153c8be282ae1236b5d33
Chaleamchai Sathanurukwong <c.sathanurukwong@gmail.com>
2023-09-30 18:21:15 -0400

    this code got top 2 horror moview 8.4

--- sql/horror_movies.sql
+++ sql/horror_movies.sql
@@ -1,2 +1,12 @@
 -- Add your SQL here
-SELECT movies.id as Movie_ID, movies.name as Movie_Title, movies.imdb_rating as Rating FROM movies WHERE genre='horror' ORDER BY movies.imdb_rating DESC LIMIT 3;
+-- SELECT movies.id as Movie_ID, movies.name as Movie_Title, movies.imdb_rating as Rating, movies.genre as Genre FROM movies WHERE genre='horror' ORDER BY movies.imdb_rating DESC LIMIT 3;
+
+
+SELECT movies.id as Movie_ID, 
+        movies.name as Movie_Title, 
+        movies.imdb_rating as Rating
+        -- movies.genre as Genre 
+    FROM movies 
+    WHERE genre='horror' 
+    ORDER BY movies.imdb_rating DESC 
+    LIMIT 3;
